db209bc24a9a80c21840a83a379e8d5b34c33949
cheapmankrabs <estauff12@gmail.com>
2020-05-21 15:42:55 -0600

    added bootstrap

--- raspberryPI/routes.py
+++ raspberryPI/routes.py
@@ -5,14 +5,14 @@ from raspberryPI import valve1
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',title="home")
 
 @app.route('/turn_on/')
 def turnOn():
     valve1.on()
-    return render_template('home.html')
+    return ''
 
 @app.route('/turn_off/')
 def turnOff():
     valve1.off()
-    return render_template('home.html')
+    return ''
