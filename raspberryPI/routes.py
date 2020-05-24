587e9f3a6e6a15b08562aa68bbe076519c63a9a7
cheapmankrabs <estauff12@gmail.com>
2020-05-24 08:45:29 -0600

    finished the weekly schedule on the homepage

--- raspberryPI/routes.py
+++ raspberryPI/routes.py
@@ -1,22 +1,27 @@
 from raspberryPI import app
 from flask import render_template, url_for, jsonify
 from raspberryPI import Valve
-from raspberryPI import valve1
+from raspberryPI import mySchedule
 
+@app.route('/home/')
 @app.route('/')
 def home():
     return render_template('home.html',title="home")
 
+@app.route('/zones/')
+def zones():
+    return render_template('zones.html', title="zones")
+
 @app.route('/turn_on/')
 def turnOn():
-    valve1.on()
+    mySchedule.valve1.on()
     return ''
 
 @app.route('/turn_off/')
 def turnOff():
-    valve1.off()
+    mySchedule.valve1.off()
     return ''
 
 @app.route('/is_day_on/<int:dayOfWeek>')
 def isDayOn(dayOfWeek):
-    return jsonify(isValveOn=valve1.getDayBoolean(dayOfWeek))
+    return jsonify(isValveOn=mySchedule.isDayOn(dayOfWeek))
