53dd7d909edadd7abd223f6ccb30cc29305c1a28
cheapmankrabs <estauff12@gmail.com>
2020-05-26 22:03:39 -0600

    added the acutal clock feature that will run when the time, day, and day line up with the schedule and the valve is on

--- raspberryPI/__init__.py
+++ raspberryPI/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask, jsonify
 from raspberryPI.Schedule import Schedule
+import multiprocessing
+from datetime import datetime as dt
+import time
 
 
 
 mySchedule = Schedule()
+def runMySchedule():
+    while True:
+        if(int(dt.now().strftime("%-H")) == mySchedule.hour and int(dt.now().strftime("%-M")) == mySchedule.minute):
+            mySchedule.runDaySchedule(int(dt.now().strftime("%w")))
+            time.sleep(2)
+        time.sleep(59)
+clock = multiprocessing.Process(target=runMySchedule)
 app = Flask(__name__)
 from raspberryPI import routes
 
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from raspberryPI import app
+from raspberryPI import app, clock
 
 if __name__ == "__main__":
+    clock.start()
     app.run(host='0.0.0.0', port=80, debug=True)
